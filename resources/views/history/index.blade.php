05ec8fb35380b8572127b56aae5d99724996b1f3
Angeline Ho <angelineho16.03@gmail.com>
2024-10-31 17:46:52 +0700

    chore: add history page

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -1,3 +1,8 @@
+@php
+use App\Enums\OrderStatus;
+use App\Enums\OrderType;
+@endphp
+
 @extends('layouts.main')
 
 @section('sidebar')
@@ -7,7 +12,54 @@
 @section('container')
 <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="container my-5 mx-3">
-        {{-- Todo[H] --}}
+        <h1 class="mb-4">History</h1>
+        @php($found = false)
+        @foreach ($orders as $order)
+            @if (in_array($order->status, [OrderStatus::COMPLETED, OrderStatus::DECLINED])) 
+                @php($found = true)
+            @endif
+        @endforeach
+        @if (!$found)
+            <div class="alert alert-danger col-md-4 p-3" role="alert">
+                <h5>There is no history</h5>
+            </div>
+        @else
+            @foreach ($orders as $order)
+                @if (in_array($order->status, [OrderStatus::COMPLETED, OrderStatus::DECLINED]))      
+                    <div class="border border-5 border-warning mt-3 p-3 pt-4 col-md-7 col-lg-10 position-relative">
+                        @foreach ($order->menus as $menu)
+                            <span class="position-absolute top-0 start-50 translate-middle badge @if($order->status == OrderStatus::DECLINED) bg-danger @else bg-success @endif px-5 py-2 fs-6">{{ ucwords($order->status) }}</span>
+                            <div class="d-flex justify-content-between mb-2">
+                                <div>
+                                    <h5 class="d-inline">{{ $menu->name }}</h5>
+                                    <h5 class="d-inline-block px-2">( Qty: {{ $menu->detail->qty }} )</h5>
+                                </div>
+                                <div>
+                                    <h5>Rp. {{ number_format($menu->price * $menu->detail->qty) }}</h5>
+                                </div>
+                            </div>
+                        @endforeach
+                        <hr class="border border-warning border-2 opacity-100">
+                        <div class="d-flex justify-content-between mt-2">
+                            <div>
+                                <h5 class="mb-1">Total</h5>
+                            </div>
+                            <div>
+                                <h5>Rp. {{ number_format($order->total) }}</h5>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between mt-2">
+                            <div>
+                                <h5 class="mt-1">Type</h5>
+                            </div>
+                            <div class="alert alert-primary w-25 text-center">
+                                <p>{{OrderType::getDescription($order->type)}}</h5>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+        @endif
     </div>
 </div>
 @endsection
